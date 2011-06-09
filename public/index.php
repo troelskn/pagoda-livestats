178dcf8e2b280cf9544b24569a1c1298fbafcc69
Troels Knak-Nielsen <troelskn@gmail.com>
2011-06-09 11:12:52 +0200

    Changed socket name for db connectivity

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <pre>
 <?php
-$db = new Pdo('mysql:dbname=marylyn;host=localhost;unix_socket=/tmp/mysql/veronica.sock', 'margaret', 'e30H79S8');
+$db = new Pdo('mysql:dbname=marylyn;host=localhost;unix_socket=/tmp/mysql/marylyn.sock', 'margaret', 'e30H79S8');
 foreach ($db->query("show databases") as $row) {
   var_dump($row);
 }
